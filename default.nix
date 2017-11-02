89ecd29c356b0af14c8546586674f7b31d726eaf
Angelo Bulfone <mbulfone@gmail.com>
2017-11-01 21:01:57 -0700

    Add default.nix file for dependencies.

---
+++ default.nix
@@ -0,0 +1,11 @@
+{pkgs ? import <nixpkgs> {}}:
+
+pkgs.rustPlatform.buildRustPackage rec {
+	name = "llang-${version}";
+	version = "0.0.1";
+	src = ./.;
+
+	cargoSha256 = "1j5vf6iy0zjr3asj9ia1bf2rqdrzz9dzf7nv1jgg5ccxjc236mfq";
+
+	buildInputs = [ pkgs.llvm_5 ];
+}
